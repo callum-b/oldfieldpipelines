d87ef394c154cededf8ac4403611547748810a46
Cal <callum.j.burnard@gmail.com>
2025-05-15 17:46:48 +0200

    Fixed MMB sscripts and added HM to andold_autodetect

--- ChIPseq/SCRIPTS/multimapbed_arm64.sh
+++ ChIPseq/SCRIPTS/multimapbed_arm64.sh
@@ -50,7 +50,7 @@ printf "Calculating intersect positions for individual vs merged peaks...\n" 1>&
 
 for a in "${peakfiles[@]}"; do
 	arra=(${a//\// })
-	aname=${arra[-1]}
+	aname=${arra[${#arra[@]}-1]} ##ZSH doesn't handle -1 apparently? So len of arr -1 for last element
 	printf "$aname\n" 1>&2
 	bedtools intersect -loj -a mmb_temp_${temp_pref}_slopped_peaks.bed -b $a | awk 'BEGIN {prev=-1} { if($2 != prev){ print $5; prev=$2 }}' > mmb_temp_${temp_pref}_intersect_slopped_$aname.bed # !! WARNING !!bedtools changed the format of their loj output, so this could be the wrong col. This script was developed with bedtools 2.31.1
 done
@@ -60,7 +60,7 @@ printf "Mapping merged peak file to each bedgraph...\n" 1>&2
 
 for b in "${graphfiles[@]}"; do
 	arrb=(${b//\// })
-	bname=${arrb[-1]}
+	bname=${arrb[${#arrb[@]}-1]}
 	printf "$bname\n" 1>&2
 	awk '!/^track/{print $1, $2, $3, ".", $4}' $b | SCRIPTS/bedmap_arm64 --prec 3 --unmapped-val 0 --wmean mmb_temp_${temp_pref}_slopped_peaks.bed /dev/stdin > mmb_temp_${temp_pref}_map_$bname.bed
 done
